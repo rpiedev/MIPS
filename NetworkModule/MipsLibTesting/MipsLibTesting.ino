09e6bc1a48ebdab49b3ececf6501f0854e25f2ec
redassser <redryan890@gmail.com>
2025-03-13 03:10:15 -0400

    test

--- NetworkModule/MipsLibTesting/MipsLibTesting.ino
+++ NetworkModule/MipsLibTesting/MipsLibTesting.ino
@@ -4,19 +4,16 @@ int base = 7;
 int arm = 6;
 int claw = 5;
 
-//MipsLab ml;
+MipsLab ml;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  //ml.Start();
+  ml.Start();
 }
 
 void loop() {
-  //ml.ElbowTo(claw, 60);
-  digitalWrite(LED_BUILTIN, HIGH);
+  ml.ElbowTo(claw, 60);
   delay(2000);
-  //ml.ElbowTo(claw, 90);
-   digitalWrite(LED_BUILTIN, LOW);
+  ml.ElbowTo(claw, 90);
   delay(2000);
 }
 
